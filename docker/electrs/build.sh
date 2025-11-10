e98d23ee71e7f564edadd0ccbf2437beddee83fc
Anucha Takham <anucha.tkhm@gmail.com>
2025-11-10 20:51:41 +0700

    explain env and adjust some variable

--- docker/electrs/build.sh
+++ docker/electrs/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+PATH_TO_DOCKER_FILE=./Dockerfile
+IMAGE_NAME=local/electrs
 VERSION=v0.10.9
+
 export VERSION=${VERSION}
-docker buildx build --build-arg VERSION -t local/electrs:${VERSION} .
+export UID=$(id -u)
+export GID=$(id -g)
+export USER=$(id -un)
+export GROUP=$(id -gn)
+
+docker buildx build -f ${PATH_TO_DOCKER_FILE} \
+--build-arg VERSION \
+--build-arg UID \
+--build-arg GID \
+--build-arg USER \
+--build-arg GROUP \
+-t ${IMAGE_NAME}:${VERSION} .
